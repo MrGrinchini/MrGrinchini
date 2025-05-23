ba99990590300e089b5d98300f7a983c0b87c680
MrGrinchini <168371363+MrGrinchini@users.noreply.github.com>
2025-05-23 20:35:19 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -4,21 +4,21 @@ var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
 var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var fullname = "Овдієнко Андрій Михайлович" // ФИО
+var name = "Андрій" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "0001837172" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "48128312341" // ИНН
 
 // Права
-var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_valid_until = "02.04.2035" // Права "Дійсні до"
 var rights_categories = "A, B" // Права "Категорії"
 var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_valid_until = "01.04.2025" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
